9b268072de51d61fce93721d01cb36edfb72fe6d
AnitaNoohian <noohian83@gmail.com>
2026-02-12 08:08:19 +0330

    add to railway server

--- db.py
+++ db.py
@@ -1,5 +1,7 @@
 import psycopg2
+import os
 
+"""
 def get_db_connection():
     return psycopg2.connect(
         host="localhost",
@@ -8,3 +10,19 @@ def get_db_connection():
         password="Anet1383",
         port=5432
     )
+    """
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+conn = psycopg2.connect(DATABASE_URL, sslmode = 'require')
+cur = conn.cursor()
+
+with open("backup.sql", "r", encoding= "utf-8") as f:
+    sql = f.read()
+
+cur.execute(sql)
+conn.commit()
+
+cur.close()
+conn.close()
+
+print("Database restored successfully!")
